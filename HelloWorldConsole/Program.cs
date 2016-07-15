a7d7b83ba0e658200cb297c1e726faefadb10264
Jason Helmus <jph290admin@gmail.com>
2016-07-15 14:24:07 -0400

    Committing the console app to SC.

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -15,44 +15,50 @@ namespace HelloWorldConsole
         {
             try
             {
+                //Load the XML config file
                 XmlDocument doc = new XmlDocument();
                 doc.Load("Settings.XML");
 
-                XmlNode uriNode = doc.SelectSingleNode("SETTINGS/API_URI");
+                XmlNode uriNode = doc.SelectSingleNode("SETTINGS/API_URI"); //Find the setting we need
 
-                if(uriNode != null)
+                if(uriNode != null) //Make sure the entry exists
                 {
-                    string uriString = uriNode.InnerText;
+                    string uriString = uriNode.InnerText; //Get the URI to call
 
-                    HttpClient hClient = new HttpClient();
+                    //Setup the http client
+                    HttpClient hClient = new HttpClient(); 
 
-                    hClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    hClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); //Setup JSON
 
-                    HttpResponseMessage response = hClient.GetAsync(uriString).Result;
-
-                    if (response.IsSuccessStatusCode)
+                    using (HttpResponseMessage response = hClient.GetAsync(uriString).Result)  //Make the call to the API & makes sure connection is closed
                     {
-
-                        var message = response.Content.ReadAsAsync(typeof(string)).Result;
-
-                        if(message != null)
+                        //Check the response from the server
+                        if (response.IsSuccessStatusCode)
                         {
-                            if (!string.IsNullOrWhiteSpace((string)message))
+                            var message = response.Content.ReadAsAsync(typeof(string)).Result; //Get the data send back from the API
+
+                            if (message != null) //Make sure there is data there
                             {
-                                Console.WriteLine("API Response: " + message);
+                                if (!string.IsNullOrWhiteSpace((string)message)) //Make sure there is something to display
+                                {
+                                    Console.WriteLine("API Response: " + message); //Write the response to the console
+                                }
                             }
                         }
-                       
+                        else
+                        {
+                            Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
-                    }
-
+                }
+                else
+                {
+                    //Setting data is missing
+                    Console.WriteLine("The Settings.XML configuration file is missing the API_URI setting.");
                 }
 
                 Console.WriteLine("Press any key to exit.");
-                Console.ReadKey();
+                Console.ReadKey(); //Wait for the user to hit a key so they can read the console
             }
             catch( Exception ex)
             {
